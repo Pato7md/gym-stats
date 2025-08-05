dfb55516a763214509373ff730e831a8f2439f9f
Pato <michael_duerrschmidt@gmx.net>
2025-08-05 12:43:46 +0200

    modify app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from dashboards.routes import gym_bp
 
 app = Flask(__name__)
-app.register_blueprint(gym_bp, url_prefix="/gym")
+app.register_blueprint(gym_bp)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
